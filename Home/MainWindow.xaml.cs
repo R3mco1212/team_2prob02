94dca138d5175e2ad873c975d227a6ab438ef9a8
Murat Varen <bebbobya@gmail.com>
2024-03-13 05:48:07 +0100

    half werkend

--- Home/MainWindow.xaml.cs
+++ Home/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Cryptography;
@@ -28,20 +29,46 @@ namespace Home
             InitializeComponent();
         }
 
+        public static byte[] ImageToByteArray(BitmapImage image)
+        {
+            byte[] imageData;
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(image));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                encoder.Save(ms);
+                imageData = ms.ToArray();
+            }
+            return imageData;
+        }
+        public static BitmapImage ByteArrayToImage(byte[] imageData)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            using(MemoryStream ms = new MemoryStream(imageData))
+            {
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.StreamSource = ms;
+                bitmap.EndInit();
+            }
+            return bitmap;
+        }
         private void BtnTest_Click(object sender, RoutedEventArgs e)
         {
-            string text = "hey i am test text";
+            BitmapImage image = new BitmapImage(new Uri("Assets/images.jpeg",UriKind.RelativeOrAbsolute));
+            byte[] imagedata = ImageToByteArray(image);
             using (Aes myAes = Aes.Create())
             {
                 // key convert to base 64
                 string keyBase64 = Convert.ToBase64String(myAes.Key);
                 // iv convert to base 64
-                byte[] encrypted = EncryptStringToByte(text, myAes.Key, myAes.IV);                
-                string decryptedText = DecryptStringFromBytes_Aes(encrypted,myAes.Key,myAes.IV);
-                MessageBox.Show(decryptedText);
+                byte[] encryptedImage = EncryptBytes(imagedata, myAes.Key, myAes.IV);                
+                byte[] decryptedImage = DecryptStringFromBytes(encryptedImage,myAes.Key,myAes.IV);
+                BitmapImage decryptedBitmapImage = ByteArrayToImage(decryptedImage);
+                imgDecrypted.Source = decryptedBitmapImage;
             }
         }
-        static byte[] EncryptStringToByte(string plainText, byte[] key, byte[] iv)
+        static byte[] EncryptBytes(byte[] plainBytes, byte[] key, byte[] iv)
         {
             byte[] encrypted;
             using (Aes aesAlg = Aes.Create())
@@ -54,38 +81,36 @@ namespace Home
                 {
                     using(CryptoStream csEncrypt = new CryptoStream(msEncrypt,encryptor, CryptoStreamMode.Write))
                     {
-                        using(StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            swEncrypt.Write(plainText);
-                        }
+                        csEncrypt.Write(plainBytes,0, plainBytes.Length);
+                        csEncrypt.FlushFinalBlock();
                         encrypted = msEncrypt.ToArray();
                     }
                 }
             }
             return encrypted;
         }
-        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        static byte[] DecryptStringFromBytes(byte[] cipherBytes, byte[] Key, byte[] IV)
         {
-            string plainText = null;
-
+            byte[] decrypted;
             using(Aes aesAlg = Aes.Create())
             {
                 aesAlg.Key = Key;
                 aesAlg.IV = IV;
                 ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-                using(MemoryStream msDecrypt =  new MemoryStream(cipherText))
+                using(MemoryStream msDecrypt =  new MemoryStream(cipherBytes))
                 {
                     using(CryptoStream csDecrypt =  new CryptoStream(msDecrypt,decryptor, CryptoStreamMode.Read))
                     {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        using(MemoryStream msPlain = new MemoryStream())
                         {
-                            plainText = srDecrypt.ReadToEnd();
+                            csDecrypt.CopyTo(msPlain);
+                            decrypted = msPlain.ToArray();
                         }
                     }
                 }
             }
-            return plainText;
+            return decrypted;
         }
     }
 }

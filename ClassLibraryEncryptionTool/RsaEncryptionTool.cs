c205022e23b3c381b5c7f36d0110f52c14900d0d
remco_ <remcodor@gmail.com>
2024-04-13 16:27:50 +0200

    rsa encryption

---
+++ ClassLibraryEncryptionTool/RsaEncryptionTool.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ClassLibraryEncryptionTool
+{
+	public class RsaEncryptionTool
+	{
+		private RSAParameters _privateKey;
+		private RSAParameters _publicKey;
+
+		public RsaEncryptionTool()
+		{
+			using (var csp = new RSACryptoServiceProvider(2048))
+			{
+				_privateKey = csp.ExportParameters(true);
+				_publicKey = csp.ExportParameters(false);
+			}
+		}
+
+		public bool SaveKeys(string folderPath, string keyName)
+		{
+
+			string publicKeyPath = System.IO.Path.Combine(folderPath, "publickey - " + keyName + ".xml");
+			string privateKeyPath = System.IO.Path.Combine(folderPath, "privatekey - " + keyName + ".xml");
+
+			if (File.Exists(publicKeyPath) || File.Exists(privateKeyPath))
+			{
+				return false;
+			}
+
+			using (var csp = new RSACryptoServiceProvider())
+			{
+				csp.ImportParameters(_publicKey);
+				string publicKeyXml = csp.ToXmlString(false);
+				System.IO.File.WriteAllText(publicKeyPath, publicKeyXml);
+			}
+
+			using (var csp = new RSACryptoServiceProvider())
+			{
+				csp.ImportParameters(_privateKey);
+				string privateKeyXml = csp.ToXmlString(true);
+				System.IO.File.WriteAllText(privateKeyPath, privateKeyXml);
+			}
+			return true;
+		}
+
+
+
+
+		public string Encrypt(string plainText, string publicKey)
+		{
+			using (var csp = new RSACryptoServiceProvider())
+			{
+				csp.FromXmlString(publicKey);
+
+				var keyBytes = Convert.FromBase64String(plainText);
+				var cypherData = csp.Encrypt(keyBytes, false);
+
+				return Convert.ToBase64String(cypherData);
+			}
+		}
+
+
+
+		public string Decrypt(string cipherText, string PrivateKey)
+		{
+			using (var csp = new RSACryptoServiceProvider())
+			{
+				try
+				{
+					csp.FromXmlString(PrivateKey);
+					var dataBytes = Convert.FromBase64String(cipherText);
+					var cipherData = csp.Decrypt(dataBytes, false);
+					return Convert.ToBase64String(cipherData);
+				}
+				catch (CryptographicException)
+				{
+					return null;
+				}
+			}
+		}
+
+
+	}
+}
--- Home/MainWindow.xaml.cs
+++ Home/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net.Mail;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,6 +20,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Home
 {
@@ -32,7 +34,7 @@ namespace Home
             InitializeComponent();
         }
 
-        private void BtnCreateKeyAndIv_Click(object sender, RoutedEventArgs e)
+		private void BtnCreateKeyAndIv_Click(object sender, RoutedEventArgs e)
         {
             Aes aes =  AesEncryptionTool.CreateAes();
             string keyBase64 = AesEncryptionTool.AesToBase64(aes.Key);
@@ -73,41 +75,24 @@ namespace Home
             }
         }
 
-        private void BtnSelectKey_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog()
-            {
-                Filter = "Selecteer een Aes Key (*.txt)|*.txt",
-                InitialDirectory = Properties.Settings.Default.DefaultFolder,
-            };
-            if(ofd.ShowDialog() == true)
-            {
-                LblKey.Content = ofd.FileName;
-            }
-            else
-            {
-                MessageBox.Show("Geen key gekozen");
-            }
-        }
+		private void BtnSelectKey_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog()
+			{
+				Filter = "Selecteer een Aes Key (*.txt)|*.txt",
+				InitialDirectory = Properties.Settings.Default.DefaultFolder,
+			};
+			if (ofd.ShowDialog() == true)
+			{
+				LblKey.Content = ofd.FileName;
+			}
+			else
+			{
+				MessageBox.Show("Geen key gekozen");
+			}
+		}
 
-        private void BtnSelectIv_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog()
-            {
-                Filter = "Selecteer een Aes Iv (*.txt)|*.txt",
-                InitialDirectory = Properties.Settings.Default.DefaultFolder,
-            };
-            if (ofd.ShowDialog() == true)
-            {
-                LblIv.Content = ofd.FileName;
-            }
-            else
-            {
-                MessageBox.Show("Geen Iv gekozen");
-            }
-        }
-
-        private void BtnSelectImage_Click(object sender, RoutedEventArgs e)
+		private void BtnSelectImage_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog()
             {
@@ -119,7 +104,25 @@ namespace Home
             }
 
         }
-        public byte[] ImageToByteArray(BitmapImage image)
+
+		private void BtnSelectIv_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog()
+			{
+				Filter = "Selecteer een Aes Iv (*.txt)|*.txt",
+				InitialDirectory = Properties.Settings.Default.DefaultFolder,
+			};
+			if (ofd.ShowDialog() == true)
+			{
+				LblIv.Content = ofd.FileName;
+			}
+			else
+			{
+				MessageBox.Show("Geen Iv gekozen");
+			}
+		}
+
+		public byte[] ImageToByteArray(BitmapImage image)
         {
             if (image == null || image.UriSource == null)
             {
@@ -148,81 +151,83 @@ namespace Home
             }
             return bitmapImage;
         }
-        private void BtnEncrypt_Click(object sender, RoutedEventArgs e)
-        {
-            if (!File.Exists(LblImagePath.Content.ToString()))
-            {
-                MessageBox.Show("Image Bestaat niet");
-                return;
-            }
-            if (!File.Exists(LblKey.Content.ToString()))
-            {
-                MessageBox.Show("Key bestaat niet");
-                return;
-            }
-            if (!File.Exists(LblIv.Content.ToString()))
-            {
-                MessageBox.Show("Iv bestaat niet");
-                return;
-            }
-            string imagePath = LblImagePath.Content.ToString();
-            BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
-            byte[] imageData = ImageToByteArray(bitmapImage);
-            string key64 = File.ReadAllText(LblKey.Content.ToString());
-            string iv64 = File.ReadAllText(LblIv.Content.ToString());
+		private void BtnEncrypt_Click(object sender, RoutedEventArgs e)
+		{
+			if (!File.Exists(LblImagePath.Content.ToString()))
+			{
+				MessageBox.Show("Image Bestaat niet");
+				return;
+			}
+			if (!File.Exists(LblKey.Content.ToString()))
+			{
+				MessageBox.Show("Key bestaat niet");
+				return;
+			}
+			if (!File.Exists(LblIv.Content.ToString()))
+			{
+				MessageBox.Show("Iv bestaat niet");
+				return;
+			}
+			string imagePath = LblImagePath.Content.ToString();
+			BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+			byte[] imageData = ImageToByteArray(bitmapImage);
+			string key64 = File.ReadAllText(LblKey.Content.ToString());
+			string iv64 = File.ReadAllText(LblIv.Content.ToString());
 
-            byte[] encryptedData = AesEncryptionTool.EncrypteByte(imageData, key64, iv64);
-            SaveFileDialog sfd = new SaveFileDialog
-            {
-                Filter = "Encrypted image data (*.txt)|*.txt",
-                InitialDirectory = Properties.Settings.Default.DefaultFolder,
-            };
+			byte[] encryptedData = AesEncryptionTool.EncrypteByte(imageData, key64, iv64);
+			SaveFileDialog sfd = new SaveFileDialog
+			{
+				Filter = "Encrypted image data (*.txt)|*.txt",
+				InitialDirectory = Properties.Settings.Default.DefaultFolder,
+			};
 
-            while (true)
-            {
-                if (sfd.ShowDialog() == true)
-                {
-                    string encryptedData64 = Convert.ToBase64String(encryptedData);
-                    File.WriteAllText(sfd.FileName, encryptedData64);
-                    MessageBox.Show("nice");
-                    break;
-                }
-            }
-        }
-
-        private void BtnDecrypt_Click(object sender, RoutedEventArgs e)
-        {
-            if (!File.Exists(LblKey.Content.ToString()))
-            {
-                MessageBox.Show("Key bestaat niet");
-                return;
-            }
-            if (!File.Exists(LblIv.Content.ToString()))
-            {
-                MessageBox.Show("Iv bestaat niet");
-                return;
-            }
-            OpenFileDialog ofd = new OpenFileDialog()
-            {
-                Filter = "Selecteer de encrypted imageDate (*.txt)|*.txt"
-            };
-            string image64Path = "";
-            if(ofd.ShowDialog() == true)
-            {
-                image64Path = ofd.FileName;
-            }
-            string image64 = File.ReadAllText(image64Path);
-            string keyPath = File.ReadAllText(LblKey.Content.ToString());
-            string ivPath = File.ReadAllText(LblIv.Content.ToString());
-            byte[] imageData = AesEncryptionTool.DecryptData(image64, keyPath, ivPath);
-
-            BitmapImage image = ByteArrayToImage(imageData);
-            ImgDecrypted.Source = image;
+			while (true)
+			{
+				if (sfd.ShowDialog() == true)
+				{
+					string encryptedData64 = Convert.ToBase64String(encryptedData);
+					File.WriteAllText(sfd.FileName, encryptedData64);
+					MessageBox.Show("nice");
+					break;
+				}
+			}
+		}
 
 
-        }
+		private void BtnDecrypt_Click(object sender, RoutedEventArgs e)
+		{
+			if (!File.Exists(LblKey.Content.ToString()))
+			{
+				MessageBox.Show("Key bestaat niet");
+				return;
+			}
+			if (!File.Exists(LblIv.Content.ToString()))
+			{
+				MessageBox.Show("Iv bestaat niet");
+				return;
+			}
+			OpenFileDialog ofd = new OpenFileDialog()
+			{
+				Filter = "Selecteer de encrypted imageDate (*.txt)|*.txt"
+			};
+			string image64Path = "";
+			if (ofd.ShowDialog() == true)
+			{
+				image64Path = ofd.FileName;
+			}
+			string image64 = File.ReadAllText(image64Path);
+			string keyPath = File.ReadAllText(LblKey.Content.ToString());
+			string ivPath = File.ReadAllText(LblIv.Content.ToString());
+			byte[] imageData = AesEncryptionTool.DecryptData(image64, keyPath, ivPath);
 
-        private void MnuDefault_Click(object sender, RoutedEventArgs e)
+			BitmapImage image = ByteArrayToImage(imageData);
+			ImgDecrypted.Source = image;
+
+
+		}
+
+
+		private void MnuDefault_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.InitialDirectory = Properties.Settings.Default.DefaultFolder;
@@ -240,5 +245,211 @@ namespace Home
                 Properties.Settings.Default.Save();
             }
         }
-    }
+
+
+		#region RSA encryptie
+
+		RsaEncryptionTool ret = new RsaEncryptionTool();
+
+		private void BtnGenerateRSAKey_Click(object sender, RoutedEventArgs e)
+		{
+			string folderName = Properties.Settings.Default.DefaultFolder;
+
+			string keyName = TbNaamKey.Text;
+
+			if (!string.IsNullOrEmpty(keyName))
+			{
+				bool keySaved = ret.SaveKeys(folderName, keyName);
+				if (keySaved == true)
+				{
+					MessageBox.Show("RSA sleutels gegenereerd en opgeslagen jou gekozen map");
+				}
+				else
+				{
+					MessageBox.Show("Er bestaan al sleutels met deze naam kies een andere naam aub");
+				}
+			}
+			else
+			{
+				MessageBox.Show("Gelieve eerst een naam te kiezen voor de sleutels");
+			}
+		}
+
+
+		private void MnuEncryptieDecryptieFolder_Click(object sender, RoutedEventArgs e)
+		{
+			var dialog = new System.Windows.Forms.FolderBrowserDialog();
+			System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+			if (result == System.Windows.Forms.DialogResult.OK)
+			{
+				Properties.Settings.Default.DefaultResultFolder = dialog.SelectedPath;
+				Properties.Settings.Default.Save();
+			}
+		}
+
+		private void BtnLaadPublicKey_Click(object sender, RoutedEventArgs e)
+		{
+			LbSleutelList.ItemsSource = null;
+			LbSleutelList.IsEnabled = true;
+			BtnDecrypteer.IsEnabled = false;
+			BtnKiesBestand.IsEnabled = true;
+			BtnSelecteerSleutel.IsEnabled = true;
+			TbNaamCipher.IsEnabled = true;
+			TbNaamPlain.IsEnabled = false;
+			LbCipherText.Foreground = new SolidColorBrush(Colors.LimeGreen);
+			LbPlainText.Foreground = new SolidColorBrush(Colors.Red);
+			string[] files = Directory.GetFiles(Properties.Settings.Default.DefaultFolder, "publickey - *.xml");
+
+			List<string> keyNames = new List<string>();
+
+			foreach (string file in files)
+			{
+				keyNames.Add(System.IO.Path.GetFileNameWithoutExtension(file));
+			}
+
+			LbSleutelList.ItemsSource = keyNames;
+
+		}
+
+		private void BtnLaadPrivateKey_Click(object sender, RoutedEventArgs e)
+		{
+			LbSleutelList.ItemsSource = null;
+			LbSleutelList.IsEnabled = true;
+			BtnEncrypteer.IsEnabled = false;
+			BtnDecrypteer.IsEnabled = false;
+			BtnKiesBestand.IsEnabled = false;
+			BtnSelecteerSleutel.IsEnabled = true;
+			TbNaamCipher.IsEnabled = false;
+			TbNaamPlain.IsEnabled = true;
+			filePath = null;
+			LbPlainText.Foreground = new SolidColorBrush(Colors.LimeGreen);
+			LbCipherText.Foreground = new SolidColorBrush(Colors.Red);
+			string[] files = Directory.GetFiles(Properties.Settings.Default.DefaultFolder, "privatekey - *.xml");
+
+			List<string> keyNames = new List<string>();
+
+			foreach (string file in files)
+			{
+				keyNames.Add(System.IO.Path.GetFileNameWithoutExtension(file));
+			}
+
+			LbSleutelList.ItemsSource = keyNames;
+
+		}
+		private string filePath;
+		private void CheckButtonEncrypt()
+		{
+			if (!string.IsNullOrEmpty(filePath) && !string.IsNullOrEmpty(selectedKeyFilePath))
+			{
+				BtnEncrypteer.IsEnabled = true;
+			}
+		}
+		private void CheckButtonDecrypt()
+		{
+			if (!string.IsNullOrEmpty(selectedKeyFilePath) && !BtnKiesBestand.IsEnabled)
+			{
+				BtnDecrypteer.IsEnabled = true;
+			}
+		}
+
+		private string selectedKeyFilePath;
+		private void BtnSelecteerSleutel_Click(object sender, RoutedEventArgs e)
+		{
+			string selectedKey = LbSleutelList.SelectedItem.ToString();
+
+			selectedKeyFilePath = System.IO.Path.Combine(Properties.Settings.Default.DefaultFolder, selectedKey + ".xml");
+			CheckButtonEncrypt();
+			CheckButtonDecrypt();
+		}
+		private void BtnKiesBestand_Click(object sender, RoutedEventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog();
+			openFileDialog.DefaultExt = ".txt";
+			openFileDialog.InitialDirectory = Properties.Settings.Default.DefaultFolder;
+			if (openFileDialog.ShowDialog() == true)
+			{
+				filePath = openFileDialog.FileName;
+				LblBestand.Content = filePath;
+				CheckButtonEncrypt();
+			}
+		}
+
+		private void BtnEncrypteer_Click(object sender, RoutedEventArgs e)
+		{
+
+			TbNaamPlain.IsEnabled = false;
+			BtnKiesBestand.IsEnabled = false;
+			string filePath = LblBestand.Content.ToString();
+			string publicKey = File.ReadAllText(selectedKeyFilePath);
+			string plainText = File.ReadAllText(filePath);
+			string resultNameC = TbNaamCipher.Text;
+			if (!string.IsNullOrEmpty(resultNameC))
+			{
+				if (!string.IsNullOrEmpty(plainText) || !string.IsNullOrEmpty(publicKey))
+				{
+					var output = ret.Encrypt(plainText, publicKey);
+					string resultFilePath = System.IO.Path.Combine(Properties.Settings.Default.DefaultResultFolder, "Encrypted - " + resultNameC + ".txt");
+					File.WriteAllText(resultFilePath, output);
+					MessageBox.Show($"Encryptie process is voltooid het versleutelde bestand bevindt zich in {resultFilePath}");
+					LbSleutelList.IsEnabled = false;
+					LbSleutelList.ItemsSource = null;
+					BtnSelecteerSleutel.IsEnabled = false;
+					BtnKiesBestand.IsEnabled = false;
+					BtnEncrypteer.IsEnabled = false;
+					LblBestand.Content = "";
+					TbNaamCipher.Text = null;
+					LbCipherText.Foreground = new SolidColorBrush(Colors.Red);
+				}
+			}
+			else
+			{
+				MessageBox.Show("vergeet geen naam te geven voor het geencrypteerde bestand");
+				LbCipherText.Foreground = new SolidColorBrush(Colors.LimeGreen);
+			}
+		}
+		private void BtnDecrypteer_Click(object sender, RoutedEventArgs e)
+		{
+			string privateKey = File.ReadAllText(selectedKeyFilePath);
+			var ofd = new OpenFileDialog();
+			ofd.InitialDirectory = Properties.Settings.Default.DefaultResultFolder;
+			if (ofd.ShowDialog() == true)
+			{
+				string file = ofd.FileName;
+				string cipherText = File.ReadAllText(file);
+				string resultNameP = TbNaamPlain.Text;
+				if (!string.IsNullOrEmpty(resultNameP))
+				{
+					if (!string.IsNullOrEmpty(cipherText) || !string.IsNullOrEmpty(privateKey))
+					{
+						var output = ret.Decrypt(cipherText, privateKey);
+						if (output != null)
+						{
+							string resultFilePath = System.IO.Path.Combine(Properties.Settings.Default.DefaultResultFolder, "Decrypted - " + resultNameP + ".txt");
+							File.WriteAllText(resultFilePath, output);
+							MessageBox.Show($"Encryptie process is voltooid het versleutelde bestand bevindt zich in {resultFilePath}");
+							LbPlainText.Foreground = new SolidColorBrush(Colors.Red);
+							TbNaamPlain.Text = "";
+							LbSleutelList.IsEnabled = false;
+							LbSleutelList = null;
+							BtnDecrypteer.IsEnabled = false;
+							TbNaamPlain.Text = null;
+						}
+						else
+						{
+							MessageBox.Show("De gekozen private key is onjuist kies een andere private key");
+							LbSleutelList.IsEnabled = false;
+						}
+					}
+				}
+				else
+				{
+					MessageBox.Show("vergeet geen naam te geven voor het gedecrypteerde bestand");
+				}
+			}
+
+		}
+
+		#endregion
+
+	}
 }
